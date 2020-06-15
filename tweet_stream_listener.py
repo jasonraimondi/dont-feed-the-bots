c38223f2fcec85c3847c7e0075cf4ff27bc9ef39
Jason Raimondi <jason@raimondi.us>
2020-06-14 23:32:16 -0700

    Add jerry-rigged tweet function with tweets going to @dontfeedthebots

--- tweet_stream_listener.py
+++ tweet_stream_listener.py
@@ -5,9 +5,15 @@ from botometer import Botometer
 from tweepy import API
 from tweepy.streaming import StreamListener
 
+
 def convert_usernames_into_ids(auth, u):
     api = API(auth)
     return list(map(lambda x: api.get_user(x).id_str, u))
+
+
+def tweet_it(auth, message, reply_status_id):
+    api = API(auth)
+    return api.update_status(message, in_reply_to_status_id="")
 
 
 class Store:
@@ -27,29 +33,33 @@ class Store:
 
 
 class TweetStreamListener(StreamListener):
-    def __init__(self, check: Store, bom: Botometer):
+    def __init__(self, store: Store, bom: Botometer, auth):
         super().__init__()
-        self.store = check
+        self.store = store
         self.bom = bom
+        self.auth = auth
 
     def on_data(self, data):
-        user_ids = self.list_mentioned_user_ids(data)
+        tweet = json.loads(data)
+        if "delete" in tweet:
+            print("skip delete tweet event")
+            return True
+
+        user_ids = self.list_mentioned_user_ids(tweet)
         if not user_ids:
             return True
 
         for screen_name, percent in self.lookup_users(user_ids):
-            message = "According to botometer, %s is %s%% likely a bot https://botometer.iuni.iu.edu" % (screen_name, percent)
+            message = "According to botometer, %s is %s%% likely a bot https://botometer.iuni.iu.edu" \
+                      % (tweet["user"]["screen_name"], percent)
+            tweet_it(self.auth, message, tweet["id"])
             print(message)
         return True
 
     def on_error(self, status):
         print(status)
 
-    def list_mentioned_user_ids(self, data):
-        tweet = json.loads(data)
-        if "delete" in tweet:
-            print("skip delete tweet event")
-            return []
+    def list_mentioned_user_ids(self, tweet):
         user_mentions = tweet["entities"]["user_mentions"]
         return list(map(lambda x: x["id"], user_mentions))
 
@@ -62,7 +72,7 @@ class TweetStreamListener(StreamListener):
 
         for uid, result in self.bom.check_accounts_in(user_ids):
             screen_name = result["user"]["screen_name"]
-            print("looking up %s--%s in botometer" %(uid, screen_name))
+            print("looking up %s--%s in botometer" % (uid, screen_name))
             percent = round(result["display_scores"]["english"] / 5, 2)
             self.store.set_user(uid, percent)
             yield uid, percent
