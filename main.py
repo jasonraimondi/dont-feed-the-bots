6e057afff722ddcce28b8f8c9e61a308c6114166
Jason Raimondi <jason@raimondi.us>
2020-06-14 22:20:35 -0700

    Add redis layer for caching users botwatch results
    Store the results for 1 week

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
 
 import botometer
-from tweepy import OAuthHandler, API, Stream
+import redis
+from tweepy import OAuthHandler, Stream
 
 from load_users_file import get_users_from_file
-from tweet_stream_listener import TweetStreamListener
+from tweet_stream_listener import TweetStreamListener, Store, convert_usernames_into_ids
 
 twitter_app_auth = {
     "consumer_key": os.environ["TW_CONSUMER_KEY"],
@@ -19,19 +20,29 @@ botometer = botometer.Botometer(wait_on_ratelimit=True,
                                 rapidapi_key=rapidapi_key,
                                 **twitter_app_auth)
 
-stream_listener = TweetStreamListener(botometer)
+store = Store(redis.Redis(host='localhost', port=6379))
+
 auth = OAuthHandler(twitter_app_auth["consumer_key"], twitter_app_auth["consumer_secret"])
 auth.set_access_token(twitter_app_auth["access_token"], twitter_app_auth["access_token_secret"])
 
+stream_listener = TweetStreamListener(store, botometer)
 
-def get_ids_for_users(users):
-    api = API(auth)
-    return list(map(lambda x: api.get_user(x).id_str, users))
+users = os.environ.get("USERS")
 
+if not users:
+    print("getting users from file")
+    users = get_users_from_file()
 
-users = get_users_from_file()
-user_ids = get_ids_for_users(users)
+if not users:
+    print("environment variable \"USERS\" or a users.json file required")
+    exit(1)
 
+if isinstance(users, str):
+    users = users.replace(" ", "").split(",")
+
+user_ids = convert_usernames_into_ids(auth, users)
+print(users)
+print(user_ids)
 stream = Stream(auth, stream_listener)
 stream.filter(follow=user_ids)
 
--- tweet_stream_listener.py
+++ tweet_stream_listener.py
@@ -1,11 +1,35 @@
 import json
+import redis
 
 from botometer import Botometer
+from tweepy import API
 from tweepy.streaming import StreamListener
+
+def convert_usernames_into_ids(auth, u):
+    api = API(auth)
+    return list(map(lambda x: api.get_user(x).id_str, u))
+
+
+class Store:
+    expires_seconds = 60 * 60 * 24 * 7
+
+    def __init__(self, r: redis.client):
+        self.redis = r
+
+    def set_user(self, user_id, value):
+        return self.redis.set(user_id, value, ex=self.expires_seconds)
+
+    def get_user(self, user_id):
+        percent = self.redis.get(user_id)
+        if not percent:
+            return None
+        return percent.decode("utf-8")
 
 
 class TweetStreamListener(StreamListener):
-    def __init__(self, bom: Botometer):
+    def __init__(self, check: Store, bom: Botometer):
+        super().__init__()
+        self.store = check
         self.bom = bom
 
     def on_data(self, data):
@@ -14,7 +38,7 @@ class TweetStreamListener(StreamListener):
             return True
 
         for screen_name, percent in self.lookup_users(user_ids):
-            message = "According to botometer, %s is %s percent likely a bot" % (screen_name, percent)
+            message = "According to botometer, %s is %s%% likely a bot https://botometer.iuni.iu.edu" % (screen_name, percent)
             print(message)
         return True
 
@@ -30,8 +54,15 @@ class TweetStreamListener(StreamListener):
         return list(map(lambda x: x["id"], user_mentions))
 
     def lookup_users(self, user_ids):
+        for uid in user_ids:
+            percent = self.store.get_user(uid)
+            if percent:
+                user_ids.remove(uid)
+                yield uid, percent
+
         for uid, result in self.bom.check_accounts_in(user_ids):
             screen_name = result["user"]["screen_name"]
-            percent = str(round(result["display_scores"]["english"] / 5, 2))
-            yield screen_name, percent
-
+            print("looking up %s--%s in botometer" %(uid, screen_name))
+            percent = round(result["display_scores"]["english"] / 5, 2)
+            self.store.set_user(uid, percent)
+            yield uid, percent
